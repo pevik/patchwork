pevik/patchwork

commit fd732ab0fc9d5b2c3d8295d750fa40f0b75c1bec

parser: Remove duplicity
commit fc1d750 copied lines added in 753e457.
Make sense to define it on single place (DRY).

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Reviewed-by: Stephen Finucane <stephen@that.guru>

patchwork/parser.py
@@ -37,6 +37,16 @@ list_id_headers = ['List-ID', 'X-Mailing-List', 'X-list']
 
 SERIES_DELAY_INTERVAL = 10
 
+# @see https://git-scm.com/docs/git-diff#_generating_patches_with_p
+EXTENDED_HEADER_LINES = ('old mode ', 'new mode ',
+        'deleted file mode ',
+        'new file mode ',
+        'copy from ', 'copy to ',
+        'rename from ', 'rename to ',
+        'similarity index ',
+        'dissimilarity index ',
+        'new file mode ', 'index ')
+
 logger = logging.getLogger(__name__)
 
 
@@ -780,17 +790,7 @@ def parse_patch(content):
             buf += line
             if line.startswith('--- '):
                 state = 2
-
-            # extended header lines
-            # @see https://git-scm.com/docs/git-diff#_generating_patches_with_p
-            if line.startswith(('old mode ', 'new mode ',
-                                'deleted file mode ',
-                                'new file mode ',
-                                'copy from ', 'copy to ',
-                                'rename from ', 'rename to ',
-                                'similarity index ',
-                                'dissimilarity index ',
-                                'new file mode ', 'index ')):
+            if line.startswith(EXTENDED_HEADER_LINES):
                 state = 6
         elif state == 2:
             if line.startswith('+++ '):
@@ -851,16 +851,7 @@ def parse_patch(content):
             else:
                 state = 5
         elif state == 6:
-            # extended header lines
-            # @see https://git-scm.com/docs/git-diff#_generating_patches_with_p
-            if line.startswith(('old mode ', 'new mode ',
-                                'deleted file mode ',
-                                'new file mode ',
-                                'copy from ', 'copy to ',
-                                'rename from ', 'rename to ',
-                                'similarity index ',
-                                'dissimilarity index ',
-                                'new file mode ', 'index ')):
+            if line.startswith(EXTENDED_HEADER_LINES):
                 patchbuf += buf + line
                 buf = ''
             elif line.startswith('--- '):

commit dfcb6512d5b16867ab50fbbcac04509c2dfa63fa

parser: Fix pep
Fixes: fd732ab ("parser: Remove duplicity")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

patchwork/parser.py
@@ -38,14 +38,15 @@ list_id_headers = ['List-ID', 'X-Mailing-List', 'X-list']
 SERIES_DELAY_INTERVAL = 10
 
 # @see https://git-scm.com/docs/git-diff#_generating_patches_with_p
-EXTENDED_HEADER_LINES = ('old mode ', 'new mode ',
-        'deleted file mode ',
-        'new file mode ',
-        'copy from ', 'copy to ',
-        'rename from ', 'rename to ',
-        'similarity index ',
-        'dissimilarity index ',
-        'new file mode ', 'index ')
+EXTENDED_HEADER_LINES = (
+    'old mode ', 'new mode ',
+    'deleted file mode ',
+    'new file mode ',
+    'copy from ', 'copy to ',
+    'rename from ', 'rename to ',
+    'similarity index ',
+    'dissimilarity index ',
+    'new file mode ', 'index ')
 
 logger = logging.getLogger(__name__)
 